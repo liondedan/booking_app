8950acffbfecbc223d10bfa595f0fc44b5344cbe
unknown <info@thinkdan.co.uk>
2017-06-19 22:23:50 +0100

    fix addition to date in controlheader

--- process/js/ControlHeader.js
+++ process/js/ControlHeader.js
@@ -4,7 +4,7 @@ var createReactClass = require('create-react-class');
 var ControlHeader = createReactClass({
   handleDateChange : function (val) {
   	var dayQuery = (this.props.dayQuery);
-  	var addDays = moment(dayQuery).add(1, 'days');
+  	var addDays = moment(dayQuery).add(val, 'days');
   	var parseAddition = moment(addDays).format("YYYY-MM-DD");
   	console.log(parseAddition);
   	this.props.changeDay(parseAddition);
@@ -15,7 +15,7 @@ var ControlHeader = createReactClass({
 		<div>
 	      <h2>{this.props.title}</h2>
 	      <h5>Today: {this.props.dayQuery}</h5>
-	      <button onClick={this.handleDateChange}>Change Date</button>
+	      <button onClick={()=>this.handleDateChange(3)}>Change Date</button>
 	 	</div>
     )
   }
