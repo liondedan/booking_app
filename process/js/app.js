560e43642cbc1b2c52f8afb298f052d0aa19cdf5
unknown <info@thinkdan.co.uk>
2017-06-20 23:43:13 +0100

    add booking basic, through modal

--- process/js/app.js
+++ process/js/app.js
@@ -4,7 +4,7 @@ var createReactClass = require('create-react-class');
 
 var ControlHeader = require('./ControlHeader');
 var BookingTable = require('./BookingTable');
-
+var AddBooking = require('./AddBooking');
 
 var MainInterface = createReactClass({
   getInitialState: function() {
@@ -12,94 +12,89 @@ var MainInterface = createReactClass({
       appTitle: "[Dynamic]",
       today: moment(new Date()).format("YYYY-MM-DD"),
       dayQuery: moment(new Date()).format("YYYY-MM-DD"),
-      data: [
-        {
-          "pitch": 1,
-          "firstName": "Buffy",
-          "email": "Hassum Harrod",
-          "arrivalDate": "2017-06-19"
-        },
-        {
-          "pitch": 1,
-          "firstName": "Spot",
-          "email": "Constance Smith",
-          "arrivalDate": "2017-06-19"
-        },
-        {
-          "pitch": 2,
-          "firstName": "Troy",
-          "email": "Constance Smith",
-          "arrivalDate": "2017-06-19"
-        },
-        {
-          "pitch": 2,
-          "firstName": "Goldie",
-          "email": "Barot Bellingham",
-          "arrivalDate": "2017-06-19"
-        },
-        {
-          "pitch": 1,
-          "firstName": "Rusty",
-          "email": "Barot Bellingham",
-          "arrivalDate": "2017-06-20"
-        },
-        {
-          "pitch": 2,
-          "firstName": "Mitten",
-          "email": "Hillary Goldwyn",
-          "arrivalDate": "2017-06-20"
-        } 
-      ],
+      data: [],
+      visibleAddBooking: false,
+      view: {showModal: false},
     } 
+  }, 
+ 
+  componentDidMount: function() {
+    this.serverRequest = $.get('./js/data.json', function(result) {
+      this.setState({
+        data: result
+      });
+    }.bind(this));
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  }, 
+
+
+  handleHideModal: function (){
+      this.setState({view: {showModal: false}})
+  },
+
+  handleShowModal: function() {
+      this.setState({view: {showModal: true}})
   },
  
-  // componentDidMount: function() {
-  //   this.serverRequest = $.get('./js/data.json', function(result) {
-  //     this.setState({
-  //       myAppointments: result
-  //     });
-  //   }.bind(this));
-  // },
-
-  // componentWillUnmount: function() {
-  //   this.serverRequest.abort();
-  // }, 
-
   _changeDay: function(date) {
     this.setState({dayQuery: date})
   },
 
+  _addBooking: function(temptBooking) {
+    var temptData = this.state.data;
+    temptData.push(temptBooking);
+    this.setState({
+      data: temptData
+    });
+  },
+
   _funcType: function (name) {
     this.setState({title:name}); 
-  }, 
-
+  },   
+ 
   _renderByQuery: function(item, index) {
     if (item.arrivalDate === this.state.dayQuery) {
       var bookingOnDay = [];
-      bookingOnDay.push(item);
+      bookingOnDay.push(item); 
       return (
          <BookingTable booking={bookingOnDay} key={index} />
       )
     }
   },
-
-  render: function() {
+ 
+  _toggleBookingVisibility: function () {
+    var tempVisState = !this.state.visibleAddBooking;
+    this.setState({
+      visibleAddBooking: tempVisState
+    });
+  },
+ 
+  render: function() { 
     bookingsObject = this.state.data.map(function(item, index) {
       return this._renderByQuery(item, index);
     }.bind(this));  
- 
+  
     return (
-        <div className="interface">
-          <ControlHeader title={ this.state.appTitle } changeDay={this._changeDay} today={this.state.today} dayQuery={this.state.dayQuery}/>
-          <ul>
-            {bookingsObject} 
-          </ul>
+        <div>
+          <div className="interface">
+            <ControlHeader title={ this.state.appTitle } changeDay={this._changeDay} today={this.state.today} dayQuery={this.state.dayQuery}/>
+            <ul>
+              {bookingsObject} 
+            </ul> 
+          </div> 
+          <div className="row">
+              <button className="btn btn-default btn-block" onClick={this.handleShowModal}>Open Modal</button>
+              {this.state.view.showModal ? <AddBooking addBooking={this._addBooking} handleHideModal={this.handleHideModal}/> : null}
+          </div>
         </div>
       ) 
-    }  
-});  
-  
-ReactDOM.render(
-  <MainInterface />, 
-  document.getElementById('app')  
-);   
+    }      
+});   
+   
+ReactDOM.render( 
+  <MainInterface />,   
+  document.getElementById('app')    
+);        
