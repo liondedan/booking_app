7058e1bc5e8fab845f6963e659f536e0b07c66be
unknown <info@thinkdan.co.uk>
2017-06-23 00:24:00 +0100

    control head component styling

--- process/js/ControlHeader.js
+++ process/js/ControlHeader.js
@@ -18,13 +18,18 @@ var ControlHeader = createReactClass({
     return (
 		<div>
 	      <h5>Today: {this.props.dayQuery}</h5>
-	      <button className="btn btn-secondary" onClick={()=>this.handleDateChange(-1)}>Previous Day</button>
-        <button className="btn btn-primary" onClick={()=>this.props.changeDay(this.props.today)}>Today</button>
-	      <button className="btn btn-secondary" onClick={()=>this.handleDateChange(1)}>Next Day</button>
+	      
 	      <div className="form-group row">
-	    	  <div className="col-10">
+	    	  <div className="col-12 col-sm-5 col-md-4 col-lg-3">
 			 			<input className="form-control" value={this.props.dayQuery} id="datePicker" type="date" name="datePicker" onChange={this.handleDatePicker}  />
 			 		</div>
+          <div className="col-12 col-sm-7 col-md-8 col-lg-6">
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button className="btn btn-secondary" onClick={()=>this.handleDateChange(-1)}>Previous Day</button>
+              <button className="btn btn-primary" onClick={()=>this.props.changeDay(this.props.today)}>Today</button>
+              <button className="btn btn-secondary" onClick={()=>this.handleDateChange(1)}>Next Day</button>
+            </div>
+          </div>
 			 	</div>
 	 	</div>
     )
