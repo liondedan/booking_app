be975d35df80770dbe8e4b9e36f2f6ffae8d6c93
unknown <info@thinkdan.co.uk>
2017-06-25 20:51:50 +0100

    render table with empty spaces

--- process/js/BookingTable/BookingRow.js
+++ process/js/BookingTable/BookingRow.js
@@ -1,16 +1,12 @@
 var React = require('react');
 
 function BookingRow (props) {
-
-		// console.log(props.booking[0]);
-
-
-		if (props.booking[0].arrivalDate !== undefined) {
-			var firstName = props.booking[0].firstName;
-			var email = props.booking[0].email;
+		if (props.booking.arrivalDate !== undefined) {
+			var firstName = props.booking.firstName;
+			var email = props.booking.email;
 			var rowStatus = _hasPaid(props);
-			var arrival = props.prettyDate(props.booking[0].arrivalDate);
-			var depart = props.prettyDate(props.booking[0].departureDate);
+			var arrival = props.prettyDate(props.booking.arrivalDate);
+			var depart = props.prettyDate(props.booking.departureDate);
 			var rowStatus = _hasPaid(props);
 			console.log(rowStatus);
 
@@ -20,7 +16,7 @@ function BookingRow (props) {
 		}  
 
 	function _hasPaid(rowData) {
-		if (rowData.booking[0].paid === "yes") {
+		if (rowData.booking.paid === "yes") {
 			return "table-success";
 		} else {
 			return "";
@@ -32,10 +28,10 @@ function BookingRow (props) {
 
 	return (
 		  <tr className={rowStatus}>
-				<th  scope="row">{props.booking[0].pitch}</th>
+				<th  scope="row">{props.booking.pitch}</th>
 				<td>{firstName}</td>
 				<td>{email}</td>
-				<td>{props.booking[0].arrivalDate}</td>
+				<td>{arrival}</td>
 				<td>{depart}</td>
 				<td style={textCenter}>
 					<i onClick={() => props.viewBooking(props.index)} className="fa fa-bars" aria-hidden="true"></i>
--- process/js/BookingTable/BookingTable.js
+++ process/js/BookingTable/BookingTable.js
@@ -38,23 +38,32 @@ var BookingTable = createReactClass({
 
     var tableArray = []; 
 
+    // loop to 20
     for (var i = 1; i < 21; i++) {
-      var isBooked = false;
+      var isBookedForDay = false;
+      // loop bookings
       for (var j = 0; j < this.props.bookingData.length; j++) {
-        if (i == this.props.bookingData[j].pitch) {
-          isBooked = true
-          break;
-        }
+        // if booking is on dayQuery
+        if (this.props.bookingData[j].departureDate > this.props.dayQuery && this.props.bookingData[j].arrivalDate <= this.props.dayQuery) {
+          // if booking matches loop index add to array 
+          if (i == this.props.bookingData[j].pitch) {
+            isBookedForDay = true
+            break;
+          }
+        } 
       }
-      if (isBooked) {
+
+      // if booking matches loop index add to array 
+      if (isBookedForDay) {
         tableArray.push(this.props.bookingData[j]);
       } else {
+        // otherwise add empty item
         tableArray.push({pitch: i});
       }
     }
 
     var rowItems = tableArray.map(function(item, index) {
-      return this._renderByQuery(item, index)
+      return <BookingRow prettyDate={this.props.prettyDate}  viewBooking={this._handleView} booking={item} key={index} index={index} />
      }.bind(this)); 
 
     console.log(tableArray);
